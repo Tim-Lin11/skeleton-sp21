b5d349a523fa7cc11ee1acf45c5f6875dc3adb8c
Tim-Lin11 <TimLinjq@gmail.com>
2024-11-21 17:08:58 +0800

    proj1

--- proj1/deque/ArrayDeque.java
+++ proj1/deque/ArrayDeque.java
@@ -2,7 +2,7 @@ package deque;
 
 import java.lang.reflect.Array;
 
-public class ArrayDeque {
+public class ArrayDeque<T> {
     private int size;
     private int capacity;
     private Object[] Array;
@@ -13,11 +13,39 @@ public class ArrayDeque {
         capacity=8;
     }
 
-    
+    public void resize(int Capacity){
+        Object[] Brray = new Object[Capacity];
+        System.arraycopy(Array,0,Brray,0,Capacity);
+        Array=Brray;
+        capacity=Capacity;
+    }
 
-    public void addLast(Object item){
+    public void addFirst(T item){
+        if(size==capacity){
+            resize(capacity*2);
+        }
+        Object[] Brray = new Object[capacity];
+        Brray[0]=item;
+        System.arraycopy(Array,0,Brray,1,size);
+        Array=Brray;
+        size++;
+    }
+
+    public void addLast(T item){
+        if(size==capacity){
+            resize(capacity*2);
+        }
         Array[size] = item;
         size++;
+    }
+
+    public void removeFirst(){
+        if (size!=0){
+            Object[] Brray = new Object[capacity];
+            System.arraycopy(Array,1,Brray,0,size);
+            Array=Brray;
+            size--;
+        }
     }
 
     public void removeLast() {
@@ -27,6 +55,17 @@ public class ArrayDeque {
         }
     }
 
+    public boolean isEmpty(){
+        return size==0;
+    }
+
+    public void printDeque(){
+        for(int i=0; i<size;i++){
+            System.out.print(Array[i]);
+        }
+        System.out.print("\n");
+    }
+
     public Object get(int Index){
         return Array[Index];
     }
@@ -34,6 +73,7 @@ public class ArrayDeque {
     public int size(){
         return size;
     }
+
 
 
 
